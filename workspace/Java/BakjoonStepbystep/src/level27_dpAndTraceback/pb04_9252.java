77ff5253b3cb10b10827fce698e149ddf5aa0eb2
TaeHwan <wjd30142@naver.com>
2022-02-03 19:13:29 +0900

    level28_dpAndTraceback 4-5

--- workspace/Java/BakjoonStepbystep/src/level27_dpAndTraceback/pb04_9252.java
+++ workspace/Java/BakjoonStepbystep/src/level27_dpAndTraceback/pb04_9252.java
@@ -1,10 +1,60 @@
 package level27_dpAndTraceback;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
 public class pb04_9252 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		char[] c1 = new char[s1.length() + 1];
+		char[] c2 = new char[s2.length() + 1];
+		for (int i = 1; i <= s1.length(); i++) {
+			c1[i] = s1.charAt(i - 1);
+		}
+		for (int i = 1; i <= s2.length(); i++) {
+			c2[i] = s2.charAt(i - 1);
+		}
 
+		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+		for (int i = 0; i <= s1.length(); i++) {
+			for (int j = 0; j <= s2.length(); j++) {
+				if (i == 0 || j == 0)
+					dp[i][j] = 0;
+				else if (c1[i] == c2[j])
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				else
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+			}
+		}
+
+//		for (int[] a : dp)
+//			System.out.println(Arrays.toString(a));
+		System.out.println(dp[s1.length()][s2.length()]);
+		Stack<Character> stack = new Stack<>();
+		int i = s1.length(), j = s2.length();
+		while (i > 0 || j > 0) {
+			if (dp[i][j] == dp[i][j - 1]) {
+				j--;
+			} else if (dp[i][j] == dp[i - 1][j]) {
+				i--;
+			} else {
+				stack.push(c1[i]);
+				i--;
+				j--;
+			}
+			if(dp[i][j] == 0) break;
+		}
+		StringBuilder sb = new StringBuilder();
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
+		System.out.println(sb);
 	}
 
 }
--- workspace/Java/BakjoonStepbystep/src/level27_dpAndTraceback/pb05_2618.java
+++ workspace/Java/BakjoonStepbystep/src/level27_dpAndTraceback/pb05_2618.java
@@ -1,10 +1,78 @@
 package level27_dpAndTraceback;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class pb05_2618 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int N, M;
+	static int[][] acci, dp;
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		acci = new int[M+1][2];
+		for (int i = 1; i <= M; i++) {
+			StringTokenizer token = new StringTokenizer(br.readLine());
+			acci[i][0] = Integer.parseInt(token.nextToken());
+			acci[i][1] = Integer.parseInt(token.nextToken());
+		}
+		dp = new int[M+1][M+1];
+
+		sb.append(cf(1, 0, 0)).append("\n");
+
+		int a1 = 0;
+		int a2 = 0;
+
+		for (int i = 1; i <= M; i++) {
+			int tmp = dist(1, a1, i);
+
+			if (dp[a1][a2] - tmp == dp[i][a2]) {
+				a1 = i;
+				sb.append("1\n");
+			} else {
+				a2 = i;
+				sb.append("2\n");
+			}
+		}
+
+		System.out.println(sb);
+	}
+
+	static int cf(int idx, int a, int b) {
+		if (idx > M)
+			return 0;
+		if (dp[a][b] != 0) {
+			return dp[a][b];
+		}
+
+		int move1 = cf(idx + 1, idx, b) + dist(1, a, idx);
+		int move2 = cf(idx + 1, a, idx) + dist(2, b, idx);
+
+		dp[a][b] = Math.min(move1, move2);
+
+		return dp[a][b];
+	}
+
+	static int dist(int num, int start, int end) {
+		int startX = acci[start][0];
+		int startY = acci[start][1];
+		int endX = acci[end][0];
+		int endY = acci[end][1];
+
+		if (start == 0) {
+			if (num == 1) {
+				startX = startY = 1;
+			} else {
+				startX = startY = N;
+			}
+		}
+
+		return Math.abs(startX - endX) + Math.abs(startY - endY);
 
 	}
-	
+
 }
