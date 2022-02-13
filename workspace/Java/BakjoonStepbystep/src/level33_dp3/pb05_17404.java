f65f7521eef83f253ac72f58708912a446648572
TaeHwan <wjd30142@naver.com>
2022-02-13 12:09:28 +0900

    level33_dp3 complete

--- workspace/Java/BakjoonStepbystep/src/level33_dp3/pb05_17404.java
+++ workspace/Java/BakjoonStepbystep/src/level33_dp3/pb05_17404.java
@@ -5,10 +5,38 @@ import java.util.*;
 
 public class pb05_17404 {
 
+	static int N, min;
+	static int[][] arr;
+	static int[][] dp;
+	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer token = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) arr[i][j] = Integer.parseInt(token.nextToken());
+		}
+		
+		int min = 1_000_000_000;
+		dp = new int[N][3];
+		for (int i = 0; i < 3; i++) { // 첫집색깔 고정시킨다
+			for(int j=0; j<N; j++) Arrays.fill(dp[j], 1_000_000_000);
+			for(int j=0; j < 3; j++)
+				if(i == j) dp[0][j] = arr[0][j];
+			
+			for (int j = 1; j < N; j++) {
+				dp[j][0] = Math.min(dp[j-1][1], dp[j-1][2]) + arr[j][0];
+				dp[j][1] = Math.min(dp[j-1][0], dp[j-1][2]) + arr[j][1];
+				dp[j][2] = Math.min(dp[j-1][0], dp[j-1][1]) + arr[j][2];
+			}
+			
+			for (int j = 0; j < 3; j++)
+				if (i != j) min = Math.min(min, dp[N - 1][j]);
+		}
+		System.out.println(min);
+		
 	}
-
 }
--- workspace/Java/BakjoonStepbystep/src/level33_dp3/pb06_2482.java
+++ workspace/Java/BakjoonStepbystep/src/level33_dp3/pb06_2482.java
@@ -5,11 +5,38 @@ import java.util.*;
 
 public class pb06_2482 {
 
+	static int MOD = 1_000_000_003;
+	static int N, K;
+	static int[][] dp;
+	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
+		if (K * 2 > N) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		dp = new int[N + 1][N + 1]; // N개의 색이 있을때 N개 선택할 수 있는 경우의 수
+		for (int i = 1; i <= N; i++) {
+			dp[i][1] = i; // 1개 선택할 수 있는 경우의수는 선택할 수 있는 색의 개수
+			dp[i][0] = 1; // 0개 선택할 수 있는 경우의수는 그 상황 자체이므로 1
+		}
 		
+		// i번째 색을 선택한경우, 선택하지않은 경우로 나누어 메모이제이션
+		for(int i = 2; i <= N; i++) {
+			for(int j=2; j<=K; j++)
+				dp[i][j] = (dp[i-1][j] + dp[i-2][j-1]) % MOD;
+		}
 		
+		// N번쨰 색을 선택한 경우와 선택하지않은 경우를 더함
+		// N번째 색을 선택한 경우 N-2가 아닌 N-3인 이유는
+		// N번째 색을 선택했으면 첫번째색은 선택할 수 없으므로 N-2개의 색에서 1을 감해주기 때문임
+		System.out.println((dp[N-3][K-1] + dp[N-1][K]) % MOD);
+		
+//		for(int[] a : dp)
+//			System.out.println(Arrays.toString(a));
 	}
 
 }
