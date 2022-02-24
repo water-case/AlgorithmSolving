2a1bea417b3d27260860658d96f495aef030dd9a
TaeHwan <wjd30142@naver.com>
2022-02-24 22:45:58 +0900

    level39_Sweeping 2-3

--- workspace/Java/BakjoonStepbystep/src/level39_Sweeping/pb02_G3_2836_수상택시.java
+++ workspace/Java/BakjoonStepbystep/src/level39_Sweeping/pb02_G3_2836_수상택시.java
@@ -3,19 +3,26 @@ package level39_Sweeping;
 import java.io.*;
 import java.util.*;
 
+/*
+ * 주어지는 쿼리수인 N이 300_000개이고
+ * M은 10억이다
+ * 결과를 저장하는 변수를 long으로 두지않으면 터짐
+ */
+
 public class pb02_G3_2836_수상택시 {
 
 	static class Line implements Comparable<Line> {
-		int x, y;
+		int end, start;
 
-		public Line(int x, int y) {
-			this.x = x;
-			this.y = y;
+		public Line(int end, int start) {
+			this.end = end;
+			this.start = start;
 		}
 
 		@Override
 		public int compareTo(Line o) {
-			return x - o.x;
+			if(end != o.end) return end - o.end;
+			return start - o.start;
 		}
 	}
 
@@ -27,27 +34,36 @@ public class pb02_G3_2836_수상택시 {
 		int N = Integer.parseInt(st.nextToken()); // 수상 택시를 타려고 하는 사람 수
 		int M = Integer.parseInt(st.nextToken()); // 상근이가 가려고 하는 집
 
-		Line[] line = new Line[N];
+		ArrayList<Line> line = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			line[i] = new Line(a, b);
+			if (a > b)
+				line.add(new Line(b, a));
+//			else
+//				line[i] = new Line(a, b);
 		}
-		Arrays.sort(line);
-		int ans = line[0].x;
-		int left = line[0].x;
-		int right = line[0].y;
-		for (int i = 1; i < N; i++) {
-			Line now = line[i];
-			if (now.x > right) { // 이어지지 않은경우
-				ans += (right - left) * 2 + (now.x - left);
-				left = now.x;
-				right = now.y;
-			} else if (now.x < left && now.y > right) // 이어진경우
-				right = now.y;
+		if(line.size() == 0) {
+			System.out.println(M);
+			System.exit(0);
 		}
-		ans += (right - left) * 2 + (M - right);
+		Collections.sort(line);
+//		for(Line l : line) System.out.println(l.start + " " + l.end);
+		long ans = M;
+		int start = line.get(0).start;
+		int end = line.get(0).end;
+		for (int i = 1; i < line.size(); i++) {
+			Line now = line.get(i);
+			if (now.end > start) { // 이어지지 않은경우
+				ans += (start - end) * 2;
+				start = now.start;
+				end = now.end;
+			} else if (now.start > start && now.end <= start) {// 이어진경우
+				start = now.start;
+			}
+		}
+		ans += (start - end) * 2;
 		System.out.println(ans);
 	}
 
--- workspace/Java/BakjoonStepbystep/src/level39_Sweeping/pb03_P4_5419_북서풍.java
+++ workspace/Java/BakjoonStepbystep/src/level39_Sweeping/pb03_P4_5419_북서풍.java
@@ -3,12 +3,85 @@ package level39_Sweeping;
 import java.io.*;
 import java.util.*;
 
-public class pb03_P4_5419_북서풍 {
+/*
+ * 수의범위는 -10억 ~ 10억이고 수의 개수는 75000이므로 좌표압축이 필수
+ * 좌표압축에 대해서 애매모호하게 생각하고 있었는데 제대로 알게됨
+ * 큰수부터 세그먼트트리에 넣으면서 숫자를 셀것이므로 압축전 y값 내림차순으로 정렬한다
+ * y축을 기준으로 좌표압축을 한다, y값이 같으면 같은 값이 들어갈것이다
+ * 이후 x좌표로는 오름차순을 해준다
+ * 세그먼트 트리에 넣는 우선순위를 정리하자면 x값이 작은것먼저, x값이 같으면 y값이 큰거부터
+ * 트리에 넣는 기준값은 압축한 y값으로 넣는다
+ * 그렇게하면 넣는 y값보다 작은 트리의 갯수는 안세고, y값보다 크면서 x값이 작은것들을 세게 되는데 문제의 목적과 정확히 부합한다 
+ */
 
+public class pb03_P4_5419_북서풍 {
+	
+	static class Pos{
+		int x, y;
+
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+	
+	static int[] tree;
+	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
+		int T=Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			int n=Integer.parseInt(br.readLine());
+			ArrayList<Pos> pl = new ArrayList<>();
+			for(int i=0; i<n; i++) {
+				st=new StringTokenizer(br.readLine(), " ");
+				pl.add(new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+			}
+			
+			Collections.sort(pl, (a,b)->b.y-a.y); // 좌표 압축전 정렬 
+			
+			int[] zip=new int[n]; // 좌표압축 시작
+			int cnt=1;
+			for(int i=0; i<n; i++) {
+				if(i>0 && pl.get(i).y!=pl.get(i-1).y) cnt++;
+				zip[i]=cnt;
+			}
+			for(int i=0; i<n; i++) pl.get(i).y=zip[i]; // 좌표압축완료
+			
+			Collections.sort(pl, (a,b)->{
+				if(a.x!=b.x) return a.x-b.x;
+				else		 return a.y-b.y;
+			});
+			
+			tree=new int[n*4];
+			long ans = 0;
+			for(int i=0; i<n; i++) {
+				ans+=query(1,n,1,1,pl.get(i).y);
+				update(1,n,1,pl.get(i).y);
+			}
+			System.out.println(ans);
+		}
+		br.close();
+	}
+	
+	static void update(int start, int end, int node, int idx) {
+		if(idx<start || end<idx) return;
+		tree[node]++;
+		if(start==end) return;
+		int mid=(start+end)/2;
+		update(start,mid,node*2,idx);
+		update(mid+1,end,node*2+1,idx);
+	}
+	
+	static int query(int start, int end, int node, int left, int right) {
+		if(left>end || right<start) return 0;
+		if(left<=start && right>=end) return tree[node];
+		int mid=(start+end)/2;
+		return query(start, mid, node*2, left, right) + query(mid+1, end, node*2+1, left, right);
 		
 	}
 
